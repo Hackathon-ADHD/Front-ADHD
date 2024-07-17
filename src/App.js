e3635ac42b1da994fa00f5c8dcac4eb4a4f283f5
Dochanii <dmlwhd010@naver.com>
2024-07-17 17:26:15 +0900

    Feat: 로그인, 회원가입 템플릿 설정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import { GlobalStyle } from "./assets/styles/globalStyle";
+
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </>
   );
 }
+
 export default App;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,44 @@
 import React from "react";
+import {
+  LoginWrapper,
+  LoginLeft,
+  LoginRight,
+  LogoWrapper,
+  Logo,
+  LoginButtonWrapper,
+  SocialLoginButton,
+  AnimationWrapper,
+} from "./LoginStyle";
+import googleSigninButton from "../../assets/images/google_signin_button.png";
+import kakaoSigninButton from "../../assets/images/kakao_signin_button.png";
+import naverSigninButton from "../../assets/images/naver_signin_button.png";
 
 const Login = () => {
-  return <div></div>;
+  return (
+    <LoginWrapper>
+      <LoginLeft>
+        <LogoWrapper>
+          <Logo>로고 설정 후 삽입 예정</Logo>
+        </LogoWrapper>
+        <LoginButtonWrapper>
+          <SocialLoginButton>
+            <img src={googleSigninButton} alt="google-login" />
+          </SocialLoginButton>
+          <SocialLoginButton className="kakao_login_button">
+            <img src={kakaoSigninButton} alt="kakao-login" />
+          </SocialLoginButton>
+          <SocialLoginButton className="naver_login_button">
+            <img src={naverSigninButton} alt="naver-login" />
+          </SocialLoginButton>
+        </LoginButtonWrapper>
+      </LoginLeft>
+      <LoginRight>
+        <AnimationWrapper>
+          여기 애들이 병 속에 들어있는 애니메이션 넣을 예정
+        </AnimationWrapper>
+      </LoginRight>
+    </LoginWrapper>
+  );
 };
 
 export default Login;
