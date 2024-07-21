ddbe02466ab3016208d71a10aa3794c6ddea1bcf
Dochanii <dmlwhd010@naver.com>
2024-07-21 17:34:33 +0900

    Feat: 메인 홈 화면 사이드 바 구성 및 홈 화면 레이아웃 작성

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,68 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { FaRegFileAlt, FaRegUser } from "react-icons/fa";
+import { FiHome } from "react-icons/fi";
+import { GrNotification } from "react-icons/gr";
+
+import {
+  HomeContainer,
+  MainWrapper,
+  SideBarButtons,
+  SideBarNickname,
+  SideBarWrapper,
+  SideBarButton,
+  NicknameText,
+  QuestionText,
+  CalendarContainer,
+  DiaryContainer,
+  SignupButton,
+  ButtonWrapper,
+} from "./HomeStyle";
 
 const Home = () => {
   const navigate = useNavigate();
   const handleClickHome = () => {
     navigate("login");
   };
+
   return (
-    <div>
-      <button onClick={handleClickHome}>signup</button>
-    </div>
+    <HomeContainer>
+      <SideBarWrapper>
+        <SideBarNickname>
+          <NicknameText>닉네임 님</NicknameText>
+          <QuestionText>오늘 하루는 어떠셨나요?</QuestionText>
+        </SideBarNickname>
+        <SideBarButtons>
+          <ButtonWrapper>
+            <SideBarButton onClick={handleClickHome}>
+              <FiHome />
+              <span>Home</span>
+            </SideBarButton>
+            <SideBarButton>
+              <FaRegFileAlt />
+              <span>일기 작성하기</span>
+            </SideBarButton>
+            <SideBarButton>
+              <GrNotification />
+              <span>알림</span>
+            </SideBarButton>
+            <SideBarButton>
+              <FaRegUser />
+              <span>My page</span>
+            </SideBarButton>
+          </ButtonWrapper>
+        </SideBarButtons>
+        <SignupButton onClick={handleClickHome}>회원가입</SignupButton>
+      </SideBarWrapper>
+      <MainWrapper>
+        <CalendarContainer>
+          <p>달력</p>
+        </CalendarContainer>
+        <DiaryContainer>
+          <p>일기</p>
+        </DiaryContainer>
+      </MainWrapper>
+    </HomeContainer>
   );
 };
 
---
+++ src/pages/Home/HomeStyle.js
@@ -0,0 +1,132 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  overflow-x: hidden;
+  display: flex;
+`;
+
+export const SideBarWrapper = styled.div`
+  width: 25%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  color: black;
+  background-color: white;
+  border-right: 1px solid #ccc;
+  padding-top: 20px;
+
+  @media (max-width: 768px) {
+    width: 25%; /* 화면이 줄어도 사이드 바의 너비가 유지되도록 설정 */
+  }
+`;
+
+export const MainWrapper = styled.div`
+  width: 75%;
+  height: 100%;
+  background-color: #d8ebf2;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+`;
+
+export const SideBarNickname = styled.div`
+  width: 100%;
+  height: 30%;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-end;
+  color: black;
+  border-bottom: 1px solid #ccc;
+`;
+
+export const NicknameText = styled.div`
+  font-size: 1.2rem;
+  font-weight: bold;
+`;
+
+export const QuestionText = styled.div`
+  font-size: 0.9rem;
+  color: #666;
+  margin-top: 5px;
+`;
+
+export const SideBarButtons = styled.div`
+  width: 100%;
+  height: 60%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-top: auto;
+  padding-bottom: 20px;
+`;
+
+export const ButtonWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const SideBarButton = styled.div`
+  width: 80%; /* Adjust the width to center within the ButtonWrapper */
+  padding: 15px 20px;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  border: 2px solid transparent;
+  transition: background-color 0.3s, border 0.3s, color 0.3s;
+
+  &:hover {
+    background-color: #e9ecef;
+    border-radius: 30px;
+    font-weight: 700;
+    color: black;
+  }
+
+  svg {
+    margin-right: 20px;
+  }
+
+  span {
+    font-size: 1rem;
+  }
+`;
+
+export const CalendarContainer = styled.div`
+  width: 80%;
+  background-color: white;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  margin-bottom: 20px;
+`;
+
+export const DiaryContainer = styled.div`
+  width: 80%;
+  background-color: white;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+`;
+
+export const SignupButton = styled.button`
+  width: 100px;
+  height: 40px;
+  background-color: black;
+  color: white;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  margin: 20px auto 0;
+  margin-bottom: 30px;
+
+  &:hover {
+    background-color: #333;
+  }
+`;
