3df551cbf8be4dc986c93da197402f6f7deb9eeb
Dochanii <dmlwhd010@naver.com>
2024-07-21 18:45:41 +0900

    Feat : 카카오 소셜 로그인 구현(정보는 서버에 저장)

---
+++ src/pages/Login/KakaoAuthRedirection.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+const KakaoAuthRedirect = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const authorizationCode = params.get("code");
+
+    if (authorizationCode) {
+      const tokenEndpoint = "https://kauth.kakao.com/oauth/token";
+      const data = new URLSearchParams({
+        grant_type: "authorization_code",
+        client_id: process.env.REACT_APP_KAKAO_CLIENT_ID,
+        redirect_uri: process.env.REACT_APP_KAKAO_REDIRECT_URI,
+        code: authorizationCode,
+      });
+
+      axios
+        .post(tokenEndpoint, data, {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        })
+        .then((response) => {
+          const accessToken = response.data.access_token;
+
+          localStorage.setItem("kakaoAccessToken", accessToken);
+
+          navigate("/signupnickname");
+        })
+        .catch((error) => {
+          console.error(
+            "Error:",
+            error.response ? error.response.data : error.message
+          );
+          if (error.response) {
+            console.error("Status:", error.response.status);
+            console.error("Headers:", error.response.headers);
+          }
+        });
+    } else {
+      console.error("No authorization code found");
+    }
+  }, [location.search, navigate]);
+
+  return <div>회원가입 진행중...</div>;
+};
+
+export default KakaoAuthRedirect;
